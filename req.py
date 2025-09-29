eff328cc8a586be78708e35de522b0caa7be9fc4
SamiAlmouss <45295635+SamiAlmouss@users.noreply.github.com>
2025-09-29 06:57:02 +0200

    Create req.py

---
+++ req.py
@@ -0,0 +1,14 @@
+import requests
+import threading
+import time
+url0 = "https://TasseoBot.onrender.com/"
+def req():
+    while True:
+        try:
+            r = requests.get(url0, timeout=100)
+            time.sleep(300)
+        except requests.RequestException as e:
+            print("Request failed:", e)
+
+t = threading.Thread(target=req)
+t.start()
