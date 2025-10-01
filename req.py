10473c9a042400b3888e10671acf8249781437fb
SamiAlmouss <45295635+SamiAlmouss@users.noreply.github.com>
2025-10-01 22:00:30 +0200

    Update req.py

--- req.py
+++ req.py
@@ -2,11 +2,13 @@ import requests
 import threading
 import time
 url0 = "https://tasseobot.onrender.com/"
+url1="÷https://quiz-flask1-ping.onrender.com"
 def req():
     while True:
         try:
             r = requests.get(url0, timeout=100)
-            time.sleep(300)
+            r = requests.get(url1, timeout=100)
+            time.sleep(900)
         except requests.RequestException as e:
             print("Request failed:", e)
 
